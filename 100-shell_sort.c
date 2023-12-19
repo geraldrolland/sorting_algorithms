326c03c8369841bb8babcb0dc81fae57439d257b
geraldrolland <geraldrolland123@gmail.com>
2023-12-19 07:56:26 -0800

    my task 100

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,30 +12,37 @@ void shell_sort(int *array, size_t size)
 {
 	int temp;
 	size_t gap, i, j;
+	int set_flag;
 
 	/* Calculate initial gap for shell sort */
-	while (gap < size / 3) {
+	while (gap < size / 3)
+	{
 		gap = gap * 3 + 1;
 	}
 
 	/* Perform shell sort */
-	while (gap > 0) {
-		for (i = gap; i < size; i++) {
+	while (gap > 0)
+	{
+		set_flag = 0;
+		for (i = gap; i < size; i++)
+		{
 			temp = array[i];
 			j = i;
 
 			/* Move elements to their correct positions within the gap */
-			while (j >= gap && array[j - gap] > temp) {
+			while (j >= gap && array[j - gap] > temp)
+			{
 				array[j] = array[j - gap];
 				j -= gap;
+				set_flag = 1;
 			}
-
 			array[j] = temp;
 		}
+		if (set_flag != 0)
+		print_array(array, size);
 
 		/* Reduce the gap and print array after each iteration */
 		gap /= 3;
-		print_array(array, size);
 	}
 }
 
