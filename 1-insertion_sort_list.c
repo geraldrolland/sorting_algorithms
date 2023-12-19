1930f28b5420cb182cab14b6dedc6b66fa16cd79
bonanwaeze@gmail.com <bonanwaeze@gmail.com>
2023-12-19 06:38:55 -0800

    sorting algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,35 +1,49 @@
 #include "sort.h"
+#include <stdio.h>
 
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ *                      in ascending order using the insertion sort algorithm
+ * @list: double pointer to the head of the list
+ *
+ * Return: void
+ */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *temp, *node, *ptr;
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
-    temp = (*list)->next;
-    
-    while (temp != NULL)
-    {
-        node = temp;
-        temp = temp->next;
+	listint_t *temp, *node, *ptr;
 
-        while (node->prev != NULL && node->n < node->prev->n)
-        {
-            ptr = node->prev;
-            
-            ptr->next = node->next;
-            if (node->next != NULL)
-                node->next->prev = ptr;
-            
-            node->prev = ptr->prev;
-            node->next = ptr;
-            
-            if (ptr->prev != NULL)
-                ptr->prev->next = node;
-            
-            ptr->prev = node;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-            if (node->prev == NULL)
-                *list = node;
-        }
-    }
-}
+	temp = (*list)->next;
+
+	while (temp != NULL)
+	{
+		node = temp;
+		temp = temp->next;
+
+		while (node->prev != NULL && node->n < node->prev->n)
+		{
+			ptr = node->prev;
+
+			/* Adjust pointers for insertion */
+			ptr->next = node->next;
+			if (node->next != NULL)
+				node->next->prev = ptr;
+
+			node->prev = ptr->prev;
+			node->next = ptr;
+
+			if (ptr->prev != NULL)
+				ptr->prev->next = node;
+
+			ptr->prev = node;
+
+			if (node->prev == NULL)
+				*list = node; /* Update head if needed */
+
+			/* Print the current state of the list */
+			print_list(*list);
+		}
+	}
+
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,27 +1,40 @@
 #include "sort.h"
 
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ *                   using the selection sort algorithm
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: void
+ */
 void selection_sort(int *array, size_t size)
 {
-    int i , j, min_val;
-    int idx;
-    for (i = 0; i < size - 1; i++)
-    {
-        idx = i;
-        min_val = array[i];
-        j = i + 1;
-        for (; j < size; j++)
-        {
-            if (array[j] < min_val)
-            {
-                min_val = array[j];
-                idx = j;
-            }
-        }
-        array[idx] = array[i];
-        if (array[i] != min_val)
-        {
-            array[i] = min_val;
-            print_array(array, size);
-        }
-    }
+	size_t i, j, min_val;
+	size_t idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		idx = i;        /* Assume the current index has the minimum value */
+                min_val = array[i]; /* Initialize minimum value */
+		j = i + 1;
+		
+		for (; j < size; j++)
+		{
+			if (array[j] < min_val)
+			{
+				min_val = array[j];
+				idx = j;
+			}
+		}
+
+		/* Swap the minimum value with the first element if needed */
+		array[idx] = array[i];
+		if (array[i] != min_val)
+		{
+			array[i] = min_val;
+			print_array(array, size); /* Print the array after each swap */
+		}
+	}
 }
+
